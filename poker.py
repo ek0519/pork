6f5857281099437f5784f8f534252b125bc05433
Ekman Hsieh <EK@Ekmans-MacBook-Pro-2.local>
2016-10-25 21:12:43 +0800

    第一次

---
+++ poker.py
@@ -0,0 +1,26 @@
+from random import shuffle, choice, sample
+heart = '♥'
+spade = '♠'
+diamond = '♦'
+club = '♣'
+poker =[]
+Card_suits = [heart, spade, diamond, club]
+
+for s in Card_suits:
+    for n in range(1,14):
+        poker.append((s,n))
+shuffle(poker)
+
+def dealer():
+    name =[]
+    for i in range(5):
+        card = poker.pop()
+        name.append(card)
+    return name
+try:
+    player01 = dealer()
+    print(player01)
+except IndexError:
+    print('牌發完了')
+
+
